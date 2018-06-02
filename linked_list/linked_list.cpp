fdb00e068c2ae0589125789cf9cfa9444d0b6653
kangaroo <Qiuwei>
2018-06-03 01:39:26 +0800

    solved some problems in tag linked list

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -16,6 +16,14 @@ public:
     }
 };
 
+void printList(ListNode *head) {
+    while(head) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
 ListNode* getMid(ListNode* head){
     if(head==NULL || head->next == NULL){
         return head;
@@ -64,6 +72,88 @@ ListNode* sortList(ListNode* head) {
         return mergeLinkedList(l, r);
 }
 
+// Problem 143 reorder list
+void reorderList(ListNode *head) {
+        if(NULL == head || NULL == head->next){ return; }
+        ListNode foo(0);
+        foo.next = head;
+        ListNode *fast(head->next), *slow(head);
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+
+        ListNode *first(head), *second(slow->next);
+        slow->next = NULL;
+
+        ListNode *old_head(second), *new_head(NULL);
+        while(second->next) {
+            old_head = second->next;
+            second->next = new_head;
+            new_head = second;
+            second = old_head;
+        }
+        second->next = new_head;
+        new_head = second;
+
+        printList(first);
+        printList(new_head);
+
+    while(first && new_head) {
+        ListNode *tmp = new_head;
+        new_head = new_head->next;
+        tmp->next = first->next;
+        first->next = tmp;
+        first = first->next->next;
+    }
+
+
+}
+
+// Problem 328 Odd even linked list
+ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode foo(0);
+        foo.next = head;
+        ListNode *odd(head), *even(head->next);
+        ListNode *odd_start(odd), *even_start(even);
+        head = head->next->next;
+
+        int cnt(3);
+
+        while(head){
+            if(cnt%2 == 0){
+                even->next = head;
+                head = head->next;
+                even = even->next;
+
+            } else {
+                odd->next = head;
+                head = head->next;
+                odd = odd->next;
+            }
+            cnt++;
+        }
+        odd->next = even_start;
+        even->next = nullptr;
+        return foo.next;
+
+
+    }
+
+ListNode* reverseLinkedList(ListNode *head) {
+        if(head == nullptr) return head;
+
+        ListNode *new_head(nullptr);
+        while(head) {
+            ListNode *tmp = head;
+            head = head->next;
+            tmp->next = new_head;
+            new_head = tmp;
+        }
+        return new_head;
+    }
 int main (int argc, char** argv)
 {
 
@@ -71,22 +161,26 @@ int main (int argc, char** argv)
     ListNode *list(foo);
     ListNode *head(foo);
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 1; i < 6; i++)
     {
         list->next = new ListNode(i);
         list = list->next;
     }
     head = head->next;
-    delete foo;
 
-    ListNode* mid = getMid(head);
-    while(mid)
-    {
-        std::cout << mid->val << " ";
-        mid = mid->next;
-    }
-    std::cout << std::endl;
+//    ListNode* mid = getMid(head);
+//    while(head)
+//    {
+//        std::cout << head->val << " ";
+//        head = head->next;
+//    }
+//    std::cout << std::endl;
 
+//    std::cout << mid->val << std::endl;
+
+    ListNode *result = reverseLinkedList(head);
+
+    printList(result);
 
     return (0);
 }
